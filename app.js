8cc93c0a6edc4f5a20469b8283956d87fd6769b6
Aadil3393 <aadil.mohan3@gmail.com>
2024-03-14 22:37:56 +0530

    Validation of schema using Joi

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema} = require("./schema.js");
 
 MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -66,8 +67,11 @@ app.get("/listings/:id", wrapAsync(async (req,res) => {
 //Create Route
 app.post("/listings", wrapAsync(async (req,res,next)=>{
     // let listing = req.body.Listing;
-        if (!req.body.listing){
-            throw new ExpressError(400,"Send valid data for listing");
+
+        let result = listingSchema.validate(req.body);
+        console.log(result);
+        if (result.error){
+            throw new ExpressError(400, result.error);
         }
         const newListing = new Listing(req.body.Listing);
         await newListing.save();
